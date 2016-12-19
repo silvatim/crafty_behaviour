eba7fea8f9c9cef2cee38db0e7fb654c6e4a3909
jamesedwardferguson <jamesedwardferguson78@gmail.com>
2016-12-19 16:22:00 +1100

    add Seed data

--- app/models/brewery.rb
+++ app/models/brewery.rb
@@ -1,3 +1,19 @@
+# == Schema Information
+#
+# Table name: breweries
+#
+#  id           :integer          not null, primary key
+#  brewery_name :string
+#  address      :string
+#  longitude    :float
+#  latitude     :float
+#  image        :string
+#  description  :text
+#  website      :string
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+#
+
 class Brewery < ActiveRecord::Base
 has_and_belongs_to_many :tours
 geocoded_by :address
