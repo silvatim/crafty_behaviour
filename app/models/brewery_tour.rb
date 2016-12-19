adf51e886e27c62df73c8849762266eb6e44d2d9
ewelina-borucka <ewelina.borucka@gmail.com>
2016-12-19 16:23:19 +1100

    change the schema

--- app/models/brewery_tour.rb
+++
@@ -1,2 +0,0 @@
-class BreweryTour < ActiveRecord::Base
-end
---
+++ db/migrate/20161219051233_breweries_tours.rb
@@ -0,0 +1,8 @@
+class BreweriesTours < ActiveRecord::Migration
+  def change
+    create_table :breweries_tours, :id => false do |t|
+      t.integer :brewery_id
+      t.integer :tour_id
+    end
+  end
+end
