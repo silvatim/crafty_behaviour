5955d734601dd667b9b8ed6aab554c1b5b71aec1
jamesedwardferguson <jamesedwardferguson78@gmail.com>
2016-12-21 09:20:22 +1100

    add backbone info rendered

---
+++ app/assets/javascripts/views/BreweryListView.js
@@ -0,0 +1,21 @@
+var app = app || {};
+
+app.BreweryListView = Backbone.View.extend({
+  tagName: "li",
+  
+  render: function(){
+    var brewery = this.model;
+    // the brewery variable is an object, includes all the lat and long for the brewery, etc.
+    var name = brewery.brewery_name;
+    // console.log(brewery);
+
+    this.$el.text(name);
+    this.$el.prependTo("#mapView");
+
+    // this.model.attributes.breweries === an array of all the associated bookings
+    // 1. create a template (in script tags in app.html.erb) for a single, list-item style brewery
+    // 2. create a js view that you can use to render them
+    // 3. iterate through each of the breweries, creating a new brewery view and rendering it.
+
+  }
+});
--- app/views/tours/_tour.json.jbuilder
+++ app/views/tours/_tour.json.jbuilder
@@ -1,3 +1,4 @@
 json.extract! tour, :id, :tour_name, :price, :num_capacity, :created_at, :updated_at
 json.url tour_url(tour, format: :json)
-json.brewery tour.brewery
+json.breweries tour.breweries
+json.tour_sessions tour.tour_sessions
