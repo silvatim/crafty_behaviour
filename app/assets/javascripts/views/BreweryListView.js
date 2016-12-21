1eb04b88b5d934b2dbb17279b3dc06897c95f068
jamesedwardferguson <jamesedwardferguson78@gmail.com>
2016-12-21 11:16:09 +1100

    add extra seed data

--- app/assets/javascripts/views/BreweryListView.js
+++ app/assets/javascripts/views/BreweryListView.js
@@ -2,16 +2,22 @@ var app = app || {};
 
 app.BreweryListView = Backbone.View.extend({
   tagName: "li",
-  
+
   render: function(){
     var brewery = this.model;
     // the brewery variable is an object, includes all the lat and long for the brewery, etc.
     var name = brewery.brewery_name;
     // console.log(brewery);
+    var maplat  = brewery.latitude;
+    var maplong = brewery.longitude;
 
     this.$el.text(name);
     this.$el.prependTo("#mapView");
 
+    // this.$el.text(maplat);
+    // this.$el.prependTo("#mapView");
+    // this.$el.text(maplong);
+    // this.$el.prependTo("#mapView");
     // this.model.attributes.breweries === an array of all the associated bookings
     // 1. create a template (in script tags in app.html.erb) for a single, list-item style brewery
     // 2. create a js view that you can use to render them
--- app/assets/javascripts/views/TourListView.js
+++ app/assets/javascripts/views/TourListView.js
@@ -6,19 +6,22 @@ app.TourListView = Backbone.View.extend({
   render: function(){
     var tour = this.model.attributes;
     var title = tour.tour_name;
-    var breweries = tour.breweries;
-    var tour_sessions = tour.tour_sessions;
+
     this.$el.text(title);
-    // console.log(content);
     this.$el.prependTo("#tourList");
   },
 
   events:{
-    'click' : 'showBrewery'
+    'click' : 'showTourInfo'
   },
 
 
-  showBrewery: function(){
+  showTourInfo: function(){
+
+    var tour = this.model.attributes;
+    var breweries = tour.breweries;
+    var tour_sessions = tour.tour_sessions;
+
 
     _.each(tour_sessions, function(tour_session) {
       var tourSessionView = new app.TourSessionView({
@@ -27,21 +30,19 @@ app.TourListView = Backbone.View.extend({
       tourSessionView.render();
     });
 
+
     _.each(breweries, function(brewery){
       var breweryView = new app.BreweryListView({
         model: brewery
       });
       breweryView.render();
     });
-  },
 
+
+  },
+});
 // this.model.attributes.breweries === an array of all the associated bookings
 // 1. create a template (in script tags in app.html.erb) for a single, list-item style brewery
 // 2. create a js view that you can use to render them
 // 3. iterate through each of the breweries, creating a new brewery view and rendering it.
 // this.model.attributes.tour_sessions === an array of all the tour sessions
-
-
-
-
-});
