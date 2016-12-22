a0f6cbfddd303dad827a585d8e8a0ae1a8efe10a
jamesedwardferguson <jamesedwardferguson78@gmail.com>
2016-12-22 15:38:57 +1100

    add premap view

--- app/assets/javascripts/views/MapView.js
+++ app/assets/javascripts/views/MapView.js
@@ -21,23 +21,23 @@ app.MapView = Backbone.View.extend({
         });
 
         var image_marker = {
-                url: 'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png',
-        //   //        // This marker is 20 pixels wide by 32 pixels high.
-                size: new google.maps.Size(20, 32),
-        //   //        // The origin for this image is (0, 0).
-                origin: new google.maps.Point(0, 0),
-        //   //        // The anchor for this image is the base of the flagpole at (0, 32).
-                anchor: new google.maps.Point(0, 32)
-               };
+            url: 'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png',
+            //   //        // This marker is 20 pixels wide by 32 pixels high.
+            size: new google.maps.Size(20, 32),
+            //   //        // The origin for this image is (0, 0).
+            origin: new google.maps.Point(0, 0),
+            //   //        // The anchor for this image is the base of the flagpole at (0, 32).
+            anchor: new google.maps.Point(0, 32)
+        };
         //       // Shapes define the clickable region of the icon. The type defines an HTML
         //       // <area> element 'poly' which traces out a polygon as a series of X,Y points.
         //       // The final coordinate closes the poly by connecting to the first coordinate.
-              var shape = {
-                coords: [1, 1, 1, 20, 18, 20, 18, 1],
-                type: 'poly'
-              };
+        var shape = {
+            coords: [1, 1, 1, 20, 18, 20, 18, 1],
+            type: 'poly'
+        };
 
-        var createMarker = function( num ) {
+        var createMarker = function(num) {
 
             console.log(breweries[num]);
 
