c0b26d94db76cf9b0df336e78184f110ccb014e5
jamesedwardferguson <jamesedwardferguson78@gmail.com>
2016-12-23 10:21:41 +1100

    fix pages.js

--- app/assets/javascripts/pages.js
+++ app/assets/javascripts/pages.js
@@ -1,22 +1,22 @@
 var app = app || {};
 
-$(document).on("ready turbolinks:load", function (){
+$(document).on("ready turbolinks:load", function() {
 
-if($("#app").length === 0){
-  return false;
+    if ($("#app").length === 0) {
+        return false;
 
-}
-
-
+    }
 
 
-  app.tours = new app.Tours();
-  app.tours.fetch().done(function(){
 
-    app.router = new app.Router();
-    Backbone.history.start();
 
-  });
+    app.tours = new app.Tours();
+    app.tours.fetch().done(function() {
+
+        app.router = new app.Router();
+        Backbone.history.start();
+
+    });
 
 
 
